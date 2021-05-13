3dc35606fdde33e3aa9c4848de6fa3aa82e7588a
Mauricio A. Valle <mauvalle@gmail.com>
2021-05-13 13:19:36 -0400

    update 130521

--- crear_train_test_function.R
+++ crear_train_test_function.R
@@ -4,15 +4,13 @@
 # inputs:
 # data: data frame de trabajo depurada
 # size: proporcion de la data para set de datos de entrenamiento.
-# train = TRUE para obtener train set, FALSE para test set.
 
-crear_train_test <- function(data, size = 0.8, train = TRUE) {
+
+crear_train_test <- function(data, size = 0.8) {
   n_row = nrow(data)
   total_row = round(size * n_row)
   train_sample  <- sample(1:n_row, total_row)
-  if (train == TRUE) {
-    return (data[train_sample, ])
-  } else {
-    return (data[-train_sample, ])
-  }
+  train_set <- data[train_sample, ]
+  test_set <- data[-train_sample, ]
+  return(list(train_set = train_set, test_set = test_set))
 }
